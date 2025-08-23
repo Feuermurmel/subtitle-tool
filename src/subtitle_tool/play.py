e1e37aae2ef892bbbc57be5b28646ca60a7c7a19
Michael Schwarz <michi.schwarz@gmail.com>
2025-08-23 15:52:44 +0200

    Automatically reload subtitles files in play command

--- src/subtitle_tool/play.py
+++ src/subtitle_tool/play.py
@@ -1,10 +1,16 @@
+import os
 import shlex
 import sys
+import time
 from pathlib import Path
 from subprocess import call
 from tempfile import TemporaryDirectory
+from threading import Event
+from threading import Thread
 
+from subtitle_tool.config import Config
 from subtitle_tool.inputs import read_input
+from subtitle_tool.srt import SRTFile
 from subtitle_tool.srt import write_srt_file
 
 
@@ -17,26 +23,62 @@ def play_command(
 ) -> None:
     with TemporaryDirectory() as temp_dir:
         temp_srt_path = Path(temp_dir) / "temp.srt"
-        srt_file, config = read_input(input_file, 0)
-        write_srt_file(temp_srt_path, srt_file, validate=False)
+
+        def update_srt_file() -> tuple[SRTFile, Config]:
+            srt_file, config = read_input(input_file, 0)
+            write_srt_file(temp_srt_path, srt_file, validate=False)
+
+            return srt_file, config
+
+        srt_file, config = update_srt_file()
 
         if delay_ms is None:
             delay_ms = config.delay_ms
 
-        cmdline = [
-            "mpv",
-            f"{video_file}",
-            "--osd-fractions",
-            "--sid=1",
-            f"--sub-file={temp_srt_path}",
-            f"--sub-delay={delay_ms / 1000:0.3f}",
-        ]
+        r, w = os.pipe()
+        w_file = os.fdopen(w, "wt")
+        thread_stop_event = Event()
 
-        if start_at_id is not None:
-            start_ms = srt_file.blocks[start_at_id - 1].from_ts_ms
-            cmdline.append(f"--start={start_ms / 1000:0.3f}")
+        def thread_target() -> None:
+            stat = None
 
-        cmdline.extend(mpv_args)
+            while not thread_stop_event.is_set():
+                time.sleep(0.5)
+                new_stat = input_file.stat()
 
-        print(f"Running: {shlex.join(cmdline)}")
-        sys.exit(call(cmdline))
+                if new_stat != stat:
+                    try:
+                        print(f"Reloading subtitles from {input_file}.")
+                        update_srt_file()
+                    except Exception as e:
+                        print(f"Error while reloading subtitles: {e}")
+
+                    print("sub-reload", file=w_file, flush=True)
+                    stat = new_stat
+
+        thread = Thread(target=thread_target)
+        thread.start()
+
+        try:
+            cmdline = [
+                "mpv",
+                f"{video_file}",
+                "--osd-level=2",
+                "--osd-fractions",
+                "--sid=1",
+                f"--sub-file={temp_srt_path}",
+                f"--sub-delay={delay_ms / 1000:0.3f}",
+                f"--input-ipc-client=fd://{r}",
+            ]
+
+            if start_at_id is not None:
+                start_ms = srt_file.blocks[start_at_id - 1].from_ts_ms
+                cmdline.append(f"--start={start_ms / 1000:0.3f}")
+
+            cmdline.extend(mpv_args)
+
+            print(f"Running: {shlex.join(cmdline)}")
+            sys.exit(call(cmdline, pass_fds=[r]))
+        finally:
+            thread_stop_event.set()
+            thread.join()
